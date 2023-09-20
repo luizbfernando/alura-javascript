92fd40fd1fe050e9712f358db197eccc99966d66
Luiz Brogiato <luiz.brogiato.silva@escola.pr.gov.br>
2023-09-20 18:19:22 -0300

    Var, let e const

---
+++ type-boolean.js
@@ -0,0 +1,14 @@
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = true;
+
+console.log(primeiroNumero === segundoNumero);  
+
+const texto1 = "Alura";
+const texto2 = "alura"; 
+
+console.log(texto1 === texto2);
+
+/** === : Simboliza a comparação no js
+ * 
+ */
