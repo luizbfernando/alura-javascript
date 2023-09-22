5ca512b1015e35724ae81a6d304b027a13741317
Luiz Brogiato <luiz.brogiato.silva@escola.pr.gov.br>
2023-09-22 14:39:16 -0300

    Aprendendo sobre operador ternário e boas práticas

---
+++ ternario.js
@@ -0,0 +1,14 @@
+/** if (idadeCliente >= idadeMinima) {
+  * console.log("Cerveja")
+  *  }
+  * else {
+  * console.log("Suco")
+  * }
+  * Uma boa prática é não usar o operador ternário quando se envolve várias linhas de código. Isto pode dificultar a leitura do código, prejudicando em futuras manutenções
+*/
+
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+             //Condição                     //True    //False
+console.log (idadeCliente >= idadeMinima ? "Cerveja" : "Suco") 
