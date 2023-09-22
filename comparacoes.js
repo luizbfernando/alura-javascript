4dac122253dd93a474e04f603e7d67972f4a77f8
Luiz Brogiato <luiz.brogiato.silva@escola.pr.gov.br>
2023-09-22 14:12:04 -0300

    Comparacoes == e ===

---
+++ comparacoes.js
@@ -0,0 +1,15 @@
+/** == -> Faz comparação inplicita
+ * 
+ */
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero === texto);
+
+//Typeof
+console.log(typeof numero);
+console.log(typeof texto);
+
+// == Compara apenas o valor, não o tipo do objeto;
+// === Compara valor e o tipo de dado
