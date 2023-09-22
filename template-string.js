9d868b1c8e0af2e80774a039cfc6e6ed35c5495d
Luiz Brogiato <luiz.brogiato.silva@escola.pr.gov.br>
2023-09-22 14:54:35 -0300

    Aprendendo sobre template sting

---
+++ template-string.js
@@ -0,0 +1,12 @@
+//Template string é uma forma mais fácil de se concatenar strings. Usa-se ${suaVariável}.
+
+const nome = "ju";
+const idade = 2021-1981;
+const cidadeNatal = "São Paulo";
+
+//const apresentacao = "Meu nome é " + nome + ", minha
+// idade é " + idade + ", e nasci na cidade de" + " São Paulo";
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeNatal}`
+
+console.log(apresentacao );
