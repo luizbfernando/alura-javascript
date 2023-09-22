f5eb7a3cb78208356b6ad24a16dce1e915f88e51
luizbfernando <luizfernandobrogiato73@gmail.com>
2023-09-22 09:33:23 -0300

    New alts

---
+++ conversao.js
@@ -0,0 +1,13 @@
+/**Conversão de tipos
+ * Conversão inplícita e explicita
+ * Number ()
+ * String ()
+ * console.log(numero == numeroString) Conversão inplícita
+ *  console.log(numero + numeroString). Concatenação indesejada
+ */
+
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero + Number(numeroString)); //Concatenação indesejada
+
---
+++ truly-falsy.js
@@ -0,0 +1,28 @@
+//Boolean
+
+const usuarioLogado = true;
+const contapaga = false;
+
+// 0 > false | 1 > true
+/*
+console.log(0 == false);
+console.log("" == false);
+console.log(1 == true);*/
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull)
+
+let numero = 3;
+let texto = "Alura"
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
+
+
+
+
+//Null
+//Undefined
