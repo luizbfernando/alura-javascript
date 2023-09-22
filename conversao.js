20d062be9c5021e9e9f957aa3bdf9aad632da3b2
Luiz Brogiato <luiz.brogiato.silva@escola.pr.gov.br>
2023-09-22 13:08:31 -0300

    New comments

--- conversao.js
+++ conversao.js
@@ -4,10 +4,12 @@
  * String ()
  * console.log(numero == numeroString) Conversão inplícita
  *  console.log(numero + numeroString). Concatenação indesejada
+ * Podemos usar o operador de soma + para fazer a conversão de textos para números, colocando-os antes das variáveis
+ * Console.log (+ largura * + altura)
+ * 
  */
 
 const numero = 456;
 const numeroString = "456";
 
 console.log(numero + Number(numeroString)); //Concatenação indesejada
-
