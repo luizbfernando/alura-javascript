e44d3b0504883f87068a3e157190cc76e2ba10c5
Luiz Brogiato <luiz.brogiato.silva@escola.pr.gov.br>
2023-09-22 14:07:34 -0300

    Aprendendo sobre consoles

---
+++ console.js
@@ -0,0 +1,13 @@
+/**Log = registro;
+ * Tratamento de erro;
+ * console.log(245);
+ * console.log("Eu sou um texto");
+ * console.log(minhaVar);
+ */
+const minhaVar = true;  
+    
+
+
+console.error('deu erro!')
+
+
