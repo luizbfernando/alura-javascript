628c0b379e07654a12109c36f000cc06b62e96c8
Luiz Brogiato <luiz.brogiato.silva@escola.pr.gov.br>
2023-09-27 17:41:28 -0300

    Buscando dados do paciente

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,15 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+
+var peso = tdPeso.textContent;
+var tdAltura = paciente.querySelector(".info-altura");
+
+var imc = peso / altura * altura; // 100
+
+
+console.log (paciente);//tr
+console.log (tdAltura);//td
+console.log (altura);//Obter 100
