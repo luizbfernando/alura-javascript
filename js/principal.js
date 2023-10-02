5fa7b48c3dfd156bdf7349d31167bd8cc05b875f
Luiz Brogiato <luiz.brogiato.silva@escola.pr.gov.br>
2023-10-02 13:54:30 -0300

    Usando operadores lógicos para fazer validação de dados

--- js/principal.js
+++ js/principal.js
@@ -7,12 +7,34 @@ var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 
 var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;  
+var altura = tdAltura.textContent;
 
+//Validação de conteúdo com operadores lógicos
 var tdImc = paciente.querySelector(".info-imc");
 
-var imc = peso / (altura * altura); // 100wd
+var pesoValido = true;
+var alturaValida = true;
 
-tdImc.textContent = imc;
+if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido");
+    pesoValido = false;
+    tdImc.textContent = "Peso inválido"
+}
+
+if (altura <= 0 || altura >= 3.00) {
+    console.log("Altura inválido");
+    alturaValida = false;
+    tdImc.textContent = "Altura inválida"
+}
+
+
+if (pesoValido && alturaValida) {
+    var imc = peso / (altura * altura); // 100wd
+    tdImc.textContent = imc;
+}
+
+
+
+
 
 
