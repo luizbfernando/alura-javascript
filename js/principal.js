c8be24dbce3e8bdef556ba22f1b7f6e9db9cec1b
Luiz <luizfernandobrogiato73@gmail.com>
2023-10-03 10:44:08 -0300

    Estilos com js

--- js/principal.js
+++ js/principal.js
@@ -24,12 +24,14 @@ for (var i = 0; i < pacientes.length; i++) {
         console.log("Peso inválido");
         pesoValido = false;
         tdImc.textContent = "Peso inválido"
+        paciente.classList.add("paciente-invalido");
     }
 
     if (altura <= 0 || altura >= 3.00) {
         console.log("Altura inválido");
         alturaValida = false;
-        tdImc.textContent = "Altura inválida"
+        tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
     }
 
 
